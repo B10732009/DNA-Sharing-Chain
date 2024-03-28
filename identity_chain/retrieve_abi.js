a7ee5bdc99193c5b10e4d680ae0b75c0a4d6a925
yuchen-Ubuntu <yuchen4667@yahoo.com.tw>
2024-03-28 14:26:25 +0800

    Add abi transformer

---
+++ identity_chain/retrieve_abi.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const abiFileDir = path.join(__dirname, 'build', 'contracts');
+const abiFileList = fs.readdirSync(abiFileDir);
+
+for (const abiFile of abiFileList) {
+    if (abiFile.length > 6 && abiFile.slice(-6) == 'abi.js') {
+        continue;
+    }
+    const abi = JSON.parse(fs.readFileSync(path.join(abiFileDir, abiFile))).abi;
+    const newAbiName = abiFile.slice(0, -5);
+    const newAbiFileName = `${newAbiName}.abi.js`;
+    const content = `const ${newAbiName}Abi = ${JSON.stringify(abi)};`;
+    fs.writeFileSync(path.join(abiFileDir, newAbiFileName), content);
+}
