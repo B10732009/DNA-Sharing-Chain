45710b121b1fd54fd700516987cf7d977c96d263
yuchen-Ubuntu <yuchen4667@yahoo.com.tw>
2024-03-31 14:41:18 +0800

    Use signed transactions to interact with contracts

--- app/routes/apply_identity.js
+++ app/routes/apply_identity.js
@@ -20,42 +20,59 @@ router.post('/', async function (req, res) {
     const id = req.body.id;
     const address = req.body.address;
     const role = req.body.role;
-    try {
-        const hashedId = crypto.createHash('sha256')
-            .update(id.toString())
-            .digest('hex');
-        console.log(hashedId);
-        const identityChainConfigPath = path.join(__dirname, '..', '..', 'config', 'identity_chain_config.json');
-        const identityChainConfig = JSON.parse(fs.readFileSync(identityChainConfigPath));
-        const contractAbiPath = path.join(__dirname, '..', '..', 'identity_chain', 'build', 'contracts', 'IdentityManager.json')
-        const contractAbi = JSON.parse(fs.readFileSync(contractAbiPath)).abi;
 
+    const hashedId = crypto.createHash('sha256')
+        .update(id.toString())
+        .digest('hex');
+    console.log(hashedId);
+    const identityChainConfigPath = path.join(__dirname, '..', '..', 'config', 'identity_chain_config.json');
+    const identityChainConfig = JSON.parse(fs.readFileSync(identityChainConfigPath));
+    const contractAbiPath = path.join(__dirname, '..', '..', 'identity_chain', 'build', 'contracts', 'IdentityManager.json')
+    const contractAbi = JSON.parse(fs.readFileSync(contractAbiPath)).abi;
+    try {
         const web3 = new Web3(identityChainConfig.url);
         const contract = new web3.eth.Contract(contractAbi, identityChainConfig.contracts.identity_manager.address);
-        const checkUserResult = await contract.methods
-            .checkUser(hashedId, address, role)
-            .call({ from: identityChainConfig.orgs.identity_chain.address });
+        const tx = {
+            gas: 2000000,
+            gasPrice: 30000000000,
+            data: contract.methods
+                .addUser(hashedId, address, role)
+                .encodeABI(),
+            from: identityChainConfig.orgs.identity_chain.address,
+            to: identityChainConfig.contracts.identity_manager.address
+        }
 
-        if (!checkUserResult['0']) {
-            console.log(`Fail to generate new idenity: ${checkUserResult['1']}`);
-            res.redirect('/apply_identity');
-        }
-        else {
-            const addUserResult = await contract.methods
-                .addUser(req.body.id, req.body.address, req.body.role)
-                .send({
-                    from: identityChainConfig.orgs.identity_chain.address,
-                    gas: 2000000,
-                    gasPrice: 30000000000
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-            console.log(addUserResult);
-            console.log('Successfully generate new identity.')
-            console.log(hashedId, address, role);
-            res.redirect('/index');
-        }
+        const signedTx = await web3.eth.accounts.signTransaction(tx, identityChainConfig.orgs.identity_chain.key);
+        await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+        const returnValues = await contract.getPastEvents('Result').returnValues;
+        console.log(returnValues);
+
+        res.redirect('/index');
+        
+        // const checkUserResult = await contract.methods
+        //     .checkUser(hashedId, address, role)
+        //     .call({ from: identityChainConfig.orgs.identity_chain.address });
+
+        // if (!checkUserResult['0']) {
+        //     console.log(`Fail to generate new idenity: ${checkUserResult['1']}`);
+        //     res.redirect('/apply_identity');
+        // }
+        // else {
+        //     const addUserResult = await contract.methods
+        //         .addUser(req.body.id, req.body.address, req.body.role)
+        //         .send({
+        //             from: identityChainConfig.orgs.identity_chain.address,
+        //             gas: 2000000,
+        //             gasPrice: 30000000000
+        //         })
+        //         .catch(function (error) {
+        //             console.log(error);
+        //         });
+        //     console.log(addUserResult);
+        //     console.log('Successfully generate new identity.')
+        //     console.log(hashedId, address, role);
+        //     res.redirect('/index');
+        // }
     }
     catch (error) {
         console.log(error);
