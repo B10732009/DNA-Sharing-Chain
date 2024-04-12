b9790dc5aeb1a7a1c67f11ea65d3f949f9cabd46
yuchen-Ubuntu <yuchen4667@yahoo.com.tw>
2024-04-13 00:13:42 +0800

    Add app chain functions

--- app_chain/chaincodes/access_control/index.js
+++ app_chain/chaincodes/access_control/index.js
@@ -11,12 +11,64 @@ class AccessControlContract extends Contract {
         // function that will be invoked on chaincode instantiation
     }
 
-    async put(ctx, key, value) {
+    async addUser(ctx, userAddress) {
+        const userInfo = {
+            permission: {
+                chr1: 0,
+                chr2: 0,
+                chr3: 0,
+                chr4: 0,
+                chr5: 0,
+                chr6: 0,
+                chr7: 0,
+                chr8: 0,
+                chr9: 0,
+                chr10: 0,
+                chr11: 0,
+                chr12: 0,
+                chr13: 0,
+                chr14: 0,
+                chr15: 0,
+                chr16: 0,
+                chr17: 0,
+                chr18: 0,
+                chr19: 0,
+                chr20: 0, 
+                chr21: 0,
+                chr22: 0,
+                chr23: 0,
+                chr24: 0
+            }
+        };
+        await ctx.stub.putState(userAddress, Buffer.from(JSON.stringify(userInfo)));
+        return { success: 'OK' };
+    }
+
+    async queryUser() {
+
+    }
+
+    async updatePermission(ctx, address, newPermission) {
+        const buffer = await ctx.stub.getState(address);
+        if (!buffer || !buffer.length) {
+            return { error: 'NOT_FOUND' };
+        }
+        let userInfo = JSON.parse(buffer);
+        user.permission = newPermission;
+        await ctx.stub.putState(userAddress, Buffer.from(JSON.stringify(userInfo)));
+        return { success: 'OK' };
+    }
+
+    async queryPermission() {
+
+    }
+
+    async _put(ctx, key, value) {
         await ctx.stub.putState(key, Buffer.from(value));
         return { success: 'OK' };
     }
 
-    async get(ctx, key) {
+    async _get(ctx, key) {
         const buffer = await ctx.stub.getState(key);
         if (!buffer || !buffer.length) {
             return { error: 'NOT_FOUND' };
@@ -24,7 +76,7 @@ class AccessControlContract extends Contract {
         return { success: 'OK', data: buffer.toString() };
     }
 
-    async query(ctx, queryString) {
+    async _query(ctx, queryString) {
         let queryIterator = await ctx.stub.getQueryResult(queryString);
         let queryResults = [];
         let iterator = await queryIterator.next();
