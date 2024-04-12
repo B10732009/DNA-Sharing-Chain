205d9d7b46323889ac97226070d04dbed161abd0
yuchen-Ubuntu <yuchen4667@yahoo.com.tw>
2024-04-13 00:13:58 +0800

    Adjust css

--- app/public/javascripts/app_manage.js
+++ app/public/javascripts/app_manage.js
@@ -1,16 +1,44 @@
-function createTable() {
+function createTable(permissionString) {
+    // parse permision
+    const permission = JSON.parse(permissionString);
+    console.log(permission);
+
+    // create table
     const tbody = document.getElementById('tbody');
     for (let i = 0; i < 23; i++) {
-        let row = tbody.insertRow();
-        let cell = row.insertCell(0);
-        cell.innerText = `#CHR${i + 1}`;
+        const row = tbody.insertRow();
+        for (let j = 0; j < 5; j++) {
+            const cell = row.insertCell(j);
+            if (j == 0) {
+                cell.innerText = `#CHR${i + 1}`;
+            }
+            else {
+                const button = document.createElement('button');
+                button.setAttribute('id', `button-${i}-${j}`);
+                button.setAttribute('class', 'form-button form-table-button-color');
+                if (j == permission[`CHR${i + 1}`]) {
+                    button.setAttribute('class', 'form-button form-table-button-selected-color');
+                }
+                else {
+                    button.setAttribute('class', 'form-button form-table-button-color');
+                }
+                cell.appendChild(button);
+            }
+        }
+    }
+
+
+}
+
+function setColor(permissionString) {
+    // set color
+    const permission = JSON.parse(permissionString);
+    for (let i = 0; i < 23; i++) {
         for (let j = 1; j < 5; j++) {
-            let cell = row.insertCell(j);
-            let button = document.createElement('button');
-            button.innerText = 'abc';
-            cell.appendChild(button);
+            document.getElementById(`button-${i}-${j}`).style['background-color'] = '#ADADAD';
         }
     }
 }
 
-createTable();
+createTable('{"CHR3":2}');
+// setColor("{}");
